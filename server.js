23486eb6cda898ab5a4d0a6e2abaec70e62e3d56
Luis Cardona <l.cardona@astoundcommerce.com>
2021-12-28 23:19:53 -0500

    video 7 done

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const router = express.Router();
 
 const app = express();
 const port = 3000;
 
-app.use('/', (req, res) => {
+app.use(router);
+
+router.get('/',(req, res) => {
   res.send('Holaaaa');
 });
+
+// app.use('/', (req, res) => {
+//   res.send('Holaaaa');
+// });
 
 app.listen(port, () => {
   console.log('listening on: http://localhost:3000/');
